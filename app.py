807fa0be128a8447bddb65059bead3304ffdabd0
Junming-Qiu <jq741@nyu.edu>
2023-04-04 05:38:43 +0000

    streamlit v1

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 import streamlit as st
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+
+models = ["cardiffnlp/twitter-xlm-roberta-base-sentiment", "nlptown/bert-base-multilingual-uncased-sentiment", "Tatyana/rubert-base-cased-sentiment-new"]
+
+
+
+st.title('Sentiment Analysis Demo')
+st.markdown("Junming Qiu")
+with st.form("form"):
+    selection = st.selectbox('Select Transformer', models)
+    text = st.text_input('Enter text: ', "I do not like to walk")
+    submitted = st.form_submit_button('Submit')
+
+    if submitted:
+        model_name = models[models.index(selection)]
+        model = AutoModelForSequenceClassification.from_pretrained(model_name)
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+        st.write(classifier(text))
--- init.bash
+++ init.bash
@@ -1,3 +1,6 @@
 sudo apt update
 sudo apt install python3-pip
 pip install streamlit
+pip install transformers datasets
+pip install torch
+pip install sentencepiece
